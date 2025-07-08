f6ede5a649e92e59c0f1f4a8f6bb5b96ccbf3c9e
Civil-py <civilmotha@gmail.com>
2025-07-08 13:00:11 +0200

    Update settings.py

--- p/p/settings.py
+++ p/p/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # this must come first
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
